3ee84f3522c5b5a58ba1ee5e20fac762d6c2c7b0
Vitalii Zakharchuk <xocoee@gmail.com>
2025-02-17 21:09:27 +0200

    add console.log

--- server.js
+++ server.js
@@ -6,16 +6,19 @@ app.use(express.json());
 let data = []
 
 app.get('/api/data', (req, res) => {
+    console.log('get request')
     res.json(data);
 });
 
 app.post('/api/data', (req, res) => {
+    console.log('post request')
     const newItem = req.body;
     data.push(newItem);
     res.status(201).json(newItem);
 });
 
 app.delete('/api/data/:index', (req, res) => {
+    console.log('delete request')
     const index = parseInt(req.params.index);
     if (index >= 0 && index < data.length) {
         const removedItem = data.splice(index, 1); 
