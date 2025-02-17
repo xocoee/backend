b41392f6f22cfa1b366f9604222df7484d729630
Vitalii Zakharchuk <xocoee@gmail.com>
2025-02-17 20:57:24 +0200

    npm init and add serveк.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let data = []
+
+app.get('/api/data', (req, res) => {
+    res.json(data);
+});
+
+app.post('/api/data', (req, res) => {
+    const newItem = req.body;
+    data.push(newItem);
+    res.status(201).json(newItem);
+});
+
+app.delete('/api/data/:index', (req, res) => {
+    const index = parseInt(req.params.index);
+    if (index >= 0 && index < data.length) {
+        const removedItem = data.splice(index, 1); 
+        res.json(removedItem);
+    } else {
+        res.status(404).json({ message: 'Item not found' });
+    }
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
