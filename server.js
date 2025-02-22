b25c32e511a2b4c96b8b41bad14e5aaccc9ac4e1
Vitalii Zakharchuk <xocoee@gmail.com>
2025-02-22 21:55:36 +0200

    update server.js

--- server.js
+++ server.js
@@ -6,24 +6,30 @@ app.use(express.json());
 let data = []
 
 app.get('/api/data', (req, res) => {
-    console.log('get request')
+    console.log('get request', data)
     res.json(data);
 });
 
 app.post('/api/data', (req, res) => {
-    console.log('post request')
+    console.log('post request', req.body)
     const newItem = req.body;
+    const newId = new Date().getTime().toString()
+    newItem.id = newId
     data.push(newItem);
+    console.log('added item', newItem);
     res.status(201).json(newItem);
 });
 
-app.delete('/api/data/:index', (req, res) => {
-    console.log('delete request')
-    const index = parseInt(req.params.index);
-    if (index >= 0 && index < data.length) {
-        const removedItem = data.splice(index, 1); 
+app.delete('/api/data/:id', (req, res) => {
+    console.log('delete request', res)
+    const { id } = req.params;
+    const index = data.findIndex(item => item.id === id);
+    if (index !== -1) {
+        const removedItem = data.splice(index, 1);
+        console.log('deleted item', removedItem);
         res.json(removedItem);
     } else {
+        console.log('Item not found with id:', id);
         res.status(404).json({ message: 'Item not found' });
     }
 });
